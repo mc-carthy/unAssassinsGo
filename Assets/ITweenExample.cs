ee3087a8a2e9e41408cdbba791852aa1474a4a90
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-02 19:46:08 +0000

    ITween testing

---
+++ Assets/ITweenExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ITweenExample : MonoBehaviour 
+{
+
+    private void Start()
+    {
+        iTween.RotateTo(gameObject, iTween.Hash(
+            "y", 90f,
+            "delay", 1f,
+            "time", 2f,
+            "easetype", iTween.EaseType.easeInBounce
+        ));
+    }
+
+}
