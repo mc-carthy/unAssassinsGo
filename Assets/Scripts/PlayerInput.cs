e2914d5a7832233bce29766d71a8983b6106dc3a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-03 19:55:09 +0000

    Create player input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour 
+{
+    private float h;
+    public float H { get { return h; }}
+
+    public float v;
+    public float V { get { return v; }}
+
+    private bool inputEnabled = false;
+    public bool InputEnabled 
+    { 
+        get { return inputEnabled; } 
+        set { inputEnabled = value; }
+    }
+
+    public void GetKeyInput()
+    {
+        if (inputEnabled)
+        {
+            h = Input.GetAxisRaw("Horizontal");
+            v = Input.GetAxisRaw("Vertical");
+        }
+    }
+}
