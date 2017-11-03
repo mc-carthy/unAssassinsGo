07dd258626ab4f58c4929f7d4a949906f46ada5f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-03 21:34:22 +0000

    Create links

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Link : MonoBehaviour 
+{
+    public float boarderWidth = 0.02f;
+    public float lineThickness = 0.5f;
+    public float scaleTime = 0.25f;
+    public float delay = 0.1f;
+    public iTween.EaseType easeType = iTween.EaseType.easeInOutExpo;
+
+    private void Start()
+    {
+        DrawLink(
+            new Vector3(4f, 0f, 0f),
+            new Vector3(4f, 0f, 2f)
+        );
+    }
+
+    public void DrawLink(Vector3 start, Vector3 end)
+    {
+        transform.localScale = new Vector3(lineThickness, 1f, 0);
+        Vector3 dir = end - start;
+        float zScale = dir.magnitude - (boarderWidth * 2);
+
+        Vector3 newScale = new Vector3(lineThickness, 1f, zScale);
+        transform.rotation = Quaternion.LookRotation(dir);
+        transform.position = start + (transform.forward * boarderWidth);
+
+        iTween.ScaleTo(gameObject, iTween.Hash(
+            "time", scaleTime,
+            "scale", newScale,
+            "easetype", easeType,
+            "delay", delay
+        ));
+    }
+}
