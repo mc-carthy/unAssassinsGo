c701c596865f24e83b53b21949aba2272c596a18
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-05 12:49:42 +0000

    Blur screen on end level

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class EndScreen : MonoBehaviour 
+{
+    public PostProcessingProfile defaultProfile;
+    public PostProcessingProfile blurProfile;
+    public PostProcessingBehaviour cameraPostProcess;
+
+    public void EnableCameraBlur(bool state)
+    {
+        if (cameraPostProcess != null && defaultProfile != null && blurProfile != null)
+        {
+            cameraPostProcess.profile = state ? blurProfile : defaultProfile;
+        }
+    }
+
+}
